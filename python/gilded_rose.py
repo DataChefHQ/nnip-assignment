4c8577ab96ca2ede0d44548102c2e906b659a203
m.semnani <moha.semnani@gmail.com>
2021-05-18 14:43:36 +0200

    GildedRose class updated to use typed items.

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -92,33 +92,20 @@ class GildedRose(object):
 
     def __init__(self, items):
         self.items = items
+        self.typed_items = []
+        for item in items:
+            if item.name.startswith("Aged Brie"):
+                self.typed_items.append(AgedBrieItem(item))
+            elif item.name.startswith("Backstage passes"):
+                self.typed_items.append(BackstagePassesItem(item))
+            elif item.name.startswith("Sulfuras"):
+                self.typed_items.append(SulfurasItem(item))
+            elif item.name.startswith("Conjured"):
+                self.typed_items.append(ConjuredItem(item))
+            else:
+                self.typed_items.append(NormalItem(item))
 
     def update_quality(self):
-        for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+        for typed_item in self.typed_items:
+            typed_item.update_sell_in()
+            typed_item.update_quality()
